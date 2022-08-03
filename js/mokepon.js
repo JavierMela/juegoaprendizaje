9feaf6f53692b03b85d6db4d2befa83e106a3045
JavierMela <javier.mela@hotmail.com>
2022-08-02 21:04:51 -0300

    Actualizacion del proyecto

---
+++ js/mokepon.js
@@ -0,0 +1,6 @@
+function seleccionarMascotajugador() {
+    alert("SELECCIONASTE TU MASCOTA")
+}
+
+let botonMascotaJugador = document.getElementById("boton-mascota")
+botonMascotaJugador.addEventListener("click", seleccionarMascotajugador)
