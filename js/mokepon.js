1a1135d1b81656ce74ecc8c72024739da05ee1da
Javier Mela <javier.mela@hotmail.com>
2022-08-03 13:32:51 -0300

    Actualizacion 03/08

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,6 +1,23 @@
-function seleccionarMascotajugador() {
-    alert("SELECCIONASTE TU MASCOTA")
+function iniciarJuego() {
+        let botonMascotaJugador = document.getElementById("boton-mascota")
+        botonMascotaJugador.addEventListener("click", seleccionarMascotajugador)
 }
 
-let botonMascotaJugador = document.getElementById("boton-mascota")
-botonMascotaJugador.addEventListener("click", seleccionarMascotajugador)
+function seleccionarMascotajugador() {
+    let inputHipodoge = document.getElementById("hipodoge")
+    let inputCapipepo = document.getElementById("capipepo")
+    let inputRatigueya = document.getElementById("ratigueya")
+
+
+    if (inputHipodoge.checked) {
+            alert("Seleccionaste a Hipodoge")     
+    }   else if (inputCapipepo.checked) {
+            alert("Seleccionaste a Capipepo")
+    }    else if (inputRatigueya.checked) {
+            alert("Seleccionaste a Ratigueya")
+    }    else {
+            alert("Selecciona una Mascota") 
+        }
+}
+
+window.addEventListener("load", iniciarJuego)
